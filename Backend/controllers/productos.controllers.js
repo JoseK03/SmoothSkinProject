10f0eb2f331bcf66ccd4bf84c45acede02daac38
JoseK03 <dajoseva@gmail.com>
2023-08-03 10:40:50 -0500

    feat: :white_check_mark: creacion del model de productos

---
+++ Backend/controllers/productos.controllers.js
@@ -0,0 +1,58 @@
+import Productos from "../models/Productos.js";
+
+const getProductos = async (req,res)=>{
+    const obtener = await Productos.find();
+    res.json(obtener);
+}
+
+const getOneProducto = async (req,res) =>{
+    try {
+        const obtenerUno = await Productos.findOne({_id:req.params.id});
+        res.json(obtenerUno);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const postProductos = async (req,res) =>{
+    const agregar = await Productos.send();
+    try {
+        const nuevo = agregar.save();
+        res.json(nuevo);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const deleteProductos = async (req,res) =>{
+    try {
+        await Productos.deleteOne({_id:req.params.id});
+        res.send();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updateProducto = async (req,res) =>{
+    try {
+        const editar = Productos.findOne({_id:req.params.id});
+        if(req.body.nombre){
+            editar.nombre = req.body.nombre
+        }
+        if(req.body.marca){
+            editar.marca = req.body.marca
+        }
+        if(req.body.descripcion){
+            editar.descripcion = req.body.descripcion
+        }
+        if(req.body.usos){
+            editar.usos = req.body.usos
+        }
+    } catch (error) {
+        
+    }
+}
+
+
+
+export {getProductos , getOneProducto, postProductos, deleteProductos, updateProducto };
